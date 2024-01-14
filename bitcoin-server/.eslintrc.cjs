ead25abaa16150db9388c2715d65f587aee6df66
Shriharsha KL <klshriharsha@gmail.com>
2024-01-14 09:53:41 +0530

    add server build step

--- bitcoin-server/.eslintrc.cjs
+++ bitcoin-server/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     env: { node: true, es2020: true },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: ['build', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['simple-import-sort'],
     rules: {
